31bb831ea6de448765ddcc617539e526c89e942f
András Veres-Szentkirályi <vsza@vsza.hu>
2010-01-08 02:51:28 +0100

    Added caching

--- index.php
+++ index.php
@@ -30,38 +30,51 @@
 $status = file_get_contents('http://vsza.hu/hacksense/status.csv');
 $status_data = explode(';', $status);
 
-$im = imagecreatetruecolor(240, 70);
-
-$red   = imagecolorallocate($im, 0xFF, 0x00, 0x00);
-$white = imagecolorallocate($im, 0xFF, 0xFF, 0xFF);
-$black = imagecolorallocate($im, 0x00, 0x00, 0x00);
-$green = imagecolorallocate($im, 0x90, 0xEE, 0x90);
-
-if (count($status_data) == 3) {
-	$open = $status_data[2][0] == 1;
-	imagefill($im, 0, 0, $open ? $green : $red);
-	// Text
-	$txt = $open ? $black : $white;
-	imagestring($im, 5, 7, 7,  'H.A.C.K. is', $txt);
-	imagestring($im, 5, 7, 27, 'currently ' . ($open ? 'OPEN' : 'CLOSED'), $txt);
-	imagestring($im, 5, 7, 47, 'since ' . $status_data[1], $txt);
-	// Lock icon -- source: Debian kde-icons-crystalproject package
-	// /usr/share/icons/crystalproject/32x32/actions/(en|de)crypted.png
-	$icon = imagecreatefrompng(($open ? 'en' : 'de') . 'crypted.png');
-	imagecopy($im, $icon, 180, 10, 0, 0, 32, 32);
-	imagedestroy($icon);
+$ok = count($status_data) == 3;
+if ($ok && file_exists('cache.id') && file_exists('cache.png')) {
+	$cache = file_get_contents('cache.id');
+	$use_cache = $cache == $status_data[0];
 } else {
-	imagefill($im, 0, 0, $red);
-	imagestring($im, 5, 7, 7, "Couldn't connect to HSAPI", $white);
+	$use_cache = false;
 }
 
-imagecolordeallocate($im, $red);
-imagecolordeallocate($im, $white);
-imagecolordeallocate($im, $black);
-imagecolordeallocate($im, $green);
+if (!$use_cache) {
+	$im = imagecreatetruecolor(240, 70);
+
+	$red   = imagecolorallocate($im, 0xFF, 0x00, 0x00);
+	$white = imagecolorallocate($im, 0xFF, 0xFF, 0xFF);
+	$black = imagecolorallocate($im, 0x00, 0x00, 0x00);
+	$green = imagecolorallocate($im, 0x90, 0xEE, 0x90);
+
+	if ($ok) {
+		$open = $status_data[2][0] == 1;
+		imagefill($im, 0, 0, $open ? $green : $red);
+		// Text
+		$txt = $open ? $black : $white;
+		imagestring($im, 5, 7, 7,  'H.A.C.K. is', $txt);
+		imagestring($im, 5, 7, 27, 'currently ' . ($open ? 'OPEN' : 'CLOSED'), $txt);
+		imagestring($im, 5, 7, 47, 'since ' . $status_data[1], $txt);
+		// Lock icon -- source: Debian kde-icons-crystalproject package
+		// /usr/share/icons/crystalproject/32x32/actions/(en|de)crypted.png
+		$icon = imagecreatefrompng(($open ? 'en' : 'de') . 'crypted.png');
+		imagecopy($im, $icon, 180, 10, 0, 0, 32, 32);
+		imagedestroy($icon);
+	} else {
+		imagefill($im, 0, 0, $red);
+		imagestring($im, 5, 7, 7, "Couldn't connect to HSAPI", $white);
+	}
+
+	imagecolordeallocate($im, $red);
+	imagecolordeallocate($im, $white);
+	imagecolordeallocate($im, $black);
+	imagecolordeallocate($im, $green);
+
+	imagepng($im, 'cache.png');
+	imagedestroy($im);
+	file_put_contents('cache.id', $status_data[0]);
+}
 
 header('Content-type: image/png');
-imagepng($im);
-imagedestroy($im);
+readfile('cache.png');
 
 ?>
